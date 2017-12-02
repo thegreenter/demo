32ca850dd2abb690971ad252892c31d6f76c1bd7
Giancarlos Salas <giansalex@gmail.com>
2017-12-02 12:42:56 -0500

    update title

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $files = array_map(function ($file) {
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Bootstrap Example</title>
+    <title>Greenter Examples</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
