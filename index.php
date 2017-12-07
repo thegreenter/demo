93a9ef47d513038b0946de99cf53b41cfba640c7
giansalex <giansalex@gmail.com>
2017-12-07 16:38:21 -0500

    add ajax

--- index.php
+++ index.php
@@ -16,8 +16,12 @@ $files = array_map(function ($file) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        ul.list-group li {
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
 
@@ -29,14 +33,37 @@ $files = array_map(function ($file) {
 <div class="container">
     <div class="row">
         <div class="col-md-4">
-            <div class="list-group">
+            <ul class="list-group">
                 <?php foreach ($files as $file): ?>
-                    <a target="_blank" href="<?=$file['path']?>" class="list-group-item"><span class="glyphicon glyphicon-menu-right"></span>&nbsp;<?=$file['name']?></a>
+                    <li onclick="loadUrl(this, '<?= $file['path']?>')" class="list-group-item"><span class="glyphicon glyphicon-menu-right"></span>&nbsp;<?=$file['name']?></li>
                 <?php endforeach; ?>
-            </div>
+            </ul>
+        </div>
+        <div class="col-md-8">
+            <div id="result" class="well">De click en algun elemento de la lista.</div>
         </div>
     </div>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script>
+    var active = null;
+    function iconLoad() {
+        $('#result').html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>');
+    }
 
+    function loadUrl(element, url) {
+        if (active) {
+            $(active).removeClass('active');
+        }
+        $(element).addClass('active');
+        active = element;
+
+        iconLoad();
+        $.get(url, function(data) {
+            $("#result").html(data);
+        });
+    }
+</script>
 </body>
 </html>
