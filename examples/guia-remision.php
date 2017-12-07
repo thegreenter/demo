37f186ba04e8a50a946004e65490e786705df49d
giansalex <giansalex@gmail.com>
2017-12-07 16:20:08 -0500

    add retencion, guia, percepcion

--- examples/guia-remision.php
+++ examples/guia-remision.php
@@ -1,1 +1,113 @@
 <?php
+
+use Greenter\Model\Client\Client;
+use Greenter\Model\Company\Address;
+use Greenter\Model\Company\Company;
+use Greenter\Model\Despatch\Despatch;
+use Greenter\Model\Despatch\DespatchDetail;
+use Greenter\Model\Despatch\Direction;
+use Greenter\Model\Despatch\Shipment;
+use Greenter\Model\Despatch\Transportist;
+use Greenter\Model\Sale\Document;
+use Greenter\Ws\Services\SunatEndpoints;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Emisor
+$address = new Address();
+$address->setUbigueo('150101')
+    ->setDepartamento('LIMA')
+    ->setProvincia('LIMA')
+    ->setDistrito('LIMA')
+    ->setUrbanizacion('NONE')
+    ->setDireccion('AV LS');
+
+$company = new Company();
+$company->setRuc('20000000001')
+    ->setRazonSocial('EMPRESA SAC')
+    ->setNombreComercial('EMPRESA')
+    ->setAddress($address);
+
+$baja = new Document();
+$baja->setTipoDoc('09')
+    ->setNroDoc('T001-00001');
+
+$rel = new Document();
+$rel->setTipoDoc('02') // Tipo: Numero de Orden de Entrega
+->setNroDoc('213123');
+
+$transp = new Transportist();
+$transp->setTipoDoc('6')
+    ->setNumDoc('20000000002')
+    ->setRznSocial('TRANSPORTES S.A.C')
+    ->setPlaca('ABI-453')
+    ->setChoferTipoDoc('1')
+    ->setChoferDoc('40003344');
+
+$envio = new Shipment();
+$envio->setModTraslado('01')
+    ->setCodTraslado('01')
+    ->setDesTraslado('VENTA')
+    ->setFecTraslado(new \DateTime())
+    ->setCodPuerto('123')
+    ->setIndTransbordo(false)
+    ->setPesoTotal(12.5)
+    ->setUndPesoTotal('KGM')
+    ->setNumBultos(2)
+    ->setNumContenedor('XD-2232')
+    ->setLlegada(new Direction('150101', 'AV LIMA'))
+    ->setPartida(new Direction('150203', 'AV ITALIA'))
+    ->setTransportista($transp);
+
+$despatch = new Despatch();
+$despatch->setTipoDoc('09')
+    ->setSerie('T001')
+    ->setCorrelativo('123')
+    ->setFechaEmision(new \DateTime())
+    ->setCompany($company)
+    ->setDestinatario((new Client())
+        ->setTipoDoc('6')
+        ->setNumDoc('20000000002')
+        ->setRznSocial('EMPRESA (<!-- --> />) 1'))
+    ->setTercero((new Client())
+        ->setTipoDoc('6')
+        ->setNumDoc('20000000003')
+        ->setRznSocial('EMPRESA SA'))
+    ->setObservacion('NOTA GUIA')
+    ->setDocBaja($baja)
+    ->setRelDoc($rel)
+    ->setEnvio($envio);
+
+$detail = new DespatchDetail();
+$detail->setCantidad(2)
+    ->setUnidad('ZZ')
+    ->setDescripcion('PROD 1')
+    ->setCodigo('PROD1');
+
+$despatch->setDetails([$detail]);
+
+// Envio a SUNAT.
+$see = new \Greenter\See();
+$see->setService(SunatEndpoints::GUIA_BETA);
+$see->setCertificate(file_get_contents(__DIR__.'/../resources/cert.pem'));
+$see->setCredentials('20000000001MODDATOS', 'moddatos');
+
+$res = $see->send($despatch);
+
+if ($res->isSuccess()) {
+    /**@var $res \Greenter\Model\Response\BillResult*/
+    $cdr = $res->getCdrResponse();
+
+    echo '<h2>Respuesta SUNAT:</h2><br>';
+    echo '<b>ID:</b> ' . $cdr->getId().'<br>';
+    echo '<b>CODE:</b> ' . $cdr->getCode().'<br>';
+    echo '<b>DESCRIPTION:</b> ' . $cdr->getDescription().'<br>';
+
+    // Descomentar para guardar el xml firmado y el CDR de respuesta.
+    //    file_put_contents('xml-signed.xml', $see->getFactory()->getLastXml());
+    //    file_put_contents('cdr.zip', $res->getCdrZip());
+} else {
+    var_dump($res->getError());
+}
+
+
--- examples/retencion.php
+++ examples/retencion.php
@@ -1,1 +1,95 @@
 <?php
+
+
+use Greenter\Model\Client\Client;
+use Greenter\Model\Company\Address;
+use Greenter\Model\Company\Company;
+use Greenter\Model\Retention\Exchange;
+use Greenter\Model\Retention\Payment;
+use Greenter\Model\Retention\Retention;
+use Greenter\Model\Retention\RetentionDetail;
+use Greenter\Ws\Services\SunatEndpoints;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Emisor
+$address = new Address();
+$address->setUbigueo('150101')
+    ->setDepartamento('LIMA')
+    ->setProvincia('LIMA')
+    ->setDistrito('LIMA')
+    ->setUrbanizacion('NONE')
+    ->setDireccion('AV LS');
+
+$company = new Company();
+$company->setRuc('20000000001')
+    ->setRazonSocial('EMPRESA SAC')
+    ->setNombreComercial('EMPRESA')
+    ->setAddress($address);
+
+$client = new Client();
+$client->setTipoDoc('6')
+    ->setNumDoc('20000000001')
+    ->setRznSocial('EMPRESA 1');
+
+$retention = new Retention();
+$retention
+    ->setSerie('R001')
+    ->setCorrelativo('123')
+    ->setFechaEmision(new \DateTime())
+    ->setCompany($company)
+    ->setProveedor($client)
+    ->setObservacion('NOTA /><!-- HI -->')
+    ->setImpRetenido(10)
+    ->setImpPagado(210)
+    ->setRegimen('01')
+    ->setTasa(3);
+
+$pay = new Payment();
+$pay->setMoneda('PEN')
+    ->setFecha(new \DateTime())
+    ->setImporte(100);
+
+$cambio = new Exchange();
+$cambio->setFecha(new \DateTime())
+    ->setFactor(1)
+    ->setMonedaObj('PEN')
+    ->setMonedaRef('PEN');
+
+$detail = new RetentionDetail();
+$detail->setTipoDoc('01')
+    ->setNumDoc('F001-1')
+    ->setFechaEmision(new \DateTime())
+    ->setFechaRetencion(new \DateTime())
+    ->setMoneda('PEN')
+    ->setImpTotal(200)
+    ->setImpPagar(200)
+    ->setImpRetenido(5)
+    ->setPagos([$pay])
+    ->setTipoCambio($cambio);
+
+$retention->setDetails([$detail]);
+
+// Envio a SUNAT.
+$see = new \Greenter\See();
+$see->setService(SunatEndpoints::RETENCION_BETA);
+$see->setCertificate(file_get_contents(__DIR__.'/../resources/cert.pem'));
+$see->setCredentials('20000000001MODDATOS', 'moddatos');
+
+$res = $see->send($retention);
+
+if ($res->isSuccess()) {
+    /**@var $res \Greenter\Model\Response\BillResult*/
+    $cdr = $res->getCdrResponse();
+
+    echo '<h2>Respuesta SUNAT:</h2><br>';
+    echo '<b>ID:</b> ' . $cdr->getId().'<br>';
+    echo '<b>CODE:</b> ' . $cdr->getCode().'<br>';
+    echo '<b>DESCRIPTION:</b> ' . $cdr->getDescription().'<br>';
+
+    // Descomentar para guardar el xml firmado y el CDR de respuesta.
+    //    file_put_contents('xml-signed.xml', $see->getFactory()->getLastXml());
+    //    file_put_contents('cdr.zip', $res->getCdrZip());
+} else {
+    var_dump($res->getError());
+}
