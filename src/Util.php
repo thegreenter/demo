2c1c95a0ea07a1fb9098f8e6cf6685b81126d4d6
Giancarlos Salas <giansalex@gmail.com>
2018-04-16 21:30:56 -0500

    remove xml-error-code-provider

--- src/Util.php
+++ src/Util.php
@@ -35,7 +35,7 @@ final class Util
     {
         $see = new See();
         $see->setService($endpoint);
-        $see->setCodeProvider(new XmlErrorCodeProvider());
+//        $see->setCodeProvider(new XmlErrorCodeProvider());
         $see->setCertificate(file_get_contents(__DIR__ . '/../resources/cert.pem'));
         $see->setCredentials('20000000001MODDATOS', 'moddatos');
         $see->setCachePath(__DIR__ . '/../cache');
