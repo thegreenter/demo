942e941a99e1e0468fc9a47e979afce84ea64876
Giancarlos Salas <giansalex@gmail.com>
2019-01-22 17:36:35 -0500

    move config docs example

---
+++ docs.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+    [
+        'file' => 'factura.php',
+        'title' => 'Factura',
+        'tags' => ['2.1'],
+    ],
+    [
+        'file' => 'factura-contingencia.php',
+        'title' => 'Factura',
+        'tags' => ['CONTINGENCIA'],
+    ],
+    [
+        'file' => 'boleta.php',
+        'title' => 'Boleta de Venta',
+        'tags' => ['2.1'],
+    ],
+    [
+        'file' => 'factura-gratuita.php',
+        'title' => 'Factura Gratuita',
+        'tags' => ['2.1'],
+    ],
+    [
+        'file' => 'factura-percepcion.php',
+        'title' => 'Factura con Percepción',
+        'tags' => ['2.1'],
+    ],
+    [
+        'file' => 'nota-credito.php',
+        'title' => 'Nota de Crédito',
+        'tags' => ['2.1'],
+    ],
+    [
+        'file' => 'nota-debito.php',
+        'title' => 'Nota de Débito',
+        'tags' => ['2.1'],
+    ],
+    [
+        'file' => 'resumen.php',
+        'title' => 'Resumen Diario',
+        'tags' => ['2.0'],
+    ],
+    [
+        'file' => 'resumen-contingencia.php',
+        'title' => 'Resumen Diario',
+        'tags' => ['CONTINGENCIA'],
+    ],
+    [
+        'file' => 'resumen-usd.php',
+        'title' => 'Resumen Diario (en Dólares)',
+        'tags' => ['2.0'],
+    ],
+    [
+        'file' => 'comunicacion-baja.php',
+        'title' => 'Comunicación de Baja',
+        'tags' => ['2.0'],
+    ],
+    [
+        'file' => 'guia-remision.php',
+        'title' => 'Guia de Remisión',
+        'tags' => ['2.1'],
+    ],
+    [
+        'file' => 'percepcion.php',
+        'title' => 'C. de Percepción',
+        'tags' => ['2.0'],
+    ],
+    [
+        'file' => 'retencion.php',
+        'title' => 'C. de Retención',
+        'tags' => ['2.0'],
+    ],
+    [
+        'file' => 'reversion.php',
+        'title' => 'Resumen de Reversiones',
+        'tags' => ['2.0'],
+    ],
+];
--- index.php
+++ index.php
@@ -1,73 +1,7 @@
 <?php
 set_time_limit(0);
 
-$docsList = [
-    [
-        'file' => 'factura.php', 
-        'title' => 'Factura',
-        'tags' => ['2.1'],
-    ],
-    [
-        'file' => 'boleta.php',
-        'title' => 'Boleta de Venta',
-        'tags' => ['2.1'],
-    ],
-    [
-        'file' => 'factura-gratuita.php',
-        'title' => 'Factura Gratuita',
-        'tags' => ['2.1'],
-    ],
-    [
-        'file' => 'factura-percepcion.php',
-        'title' => 'Factura con Percepción',
-        'tags' => ['2.1'],
-    ],
-    [
-        'file' => 'nota-credito.php',
-        'title' => 'Nota de Crédito',
-        'tags' => ['2.1'],
-    ],
-    [
-        'file' => 'nota-debito.php',
-        'title' => 'Nota de Débito',
-        'tags' => ['2.1'],
-    ],
-    [
-        'file' => 'resumen.php',
-        'title' => 'Resumen Diario',
-        'tags' => ['2.0'],
-    ],
-    [
-        'file' => 'resumen-usd.php',
-        'title' => 'Resumen Diario (en Dólares)',
-        'tags' => ['2.0'],
-    ],
-    [
-        'file' => 'comunicacion-baja.php',
-        'title' => 'Comunicación de Baja',
-        'tags' => ['2.0'],
-    ],
-    [
-        'file' => 'guia-remision.php',
-        'title' => 'Guia de Remisión',
-        'tags' => ['2.1'],
-    ],
-    [
-        'file' => 'percepcion.php',
-        'title' => 'C. de Percepción',
-        'tags' => ['2.0'],
-    ],
-    [
-        'file' => 'retencion.php',
-        'title' => 'C. de Retención',
-        'tags' => ['2.0'],
-    ],
-    [
-        'file' => 'reversion.php',
-        'title' => 'Resumen de Reversiones',
-        'tags' => ['2.0'],
-    ],
-];
+$docsList = require __DIR__.'/docs.php';
 
 $pdfPaths = glob(__DIR__.'/examples/report/*.php');
 $pdfPaths = array_map(function ($file) {
