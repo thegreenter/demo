5d92aac4d056d3e5ed9f8f63bde791183ce7ff61
Giancarlos Salas <giansalex@gmail.com>
2019-01-22 17:34:59 -0500

    add factura contingencia

---
+++ examples/factura-contingencia.php
@@ -0,0 +1,66 @@
+<?php
+
+use Greenter\Model\Sale\Invoice;
+use Greenter\Model\Sale\SaleDetail;
+use Greenter\Model\Sale\Legend;
+use Greenter\Ws\Services\SunatEndpoints;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$util = Util::getInstance();
+
+$invoice = new Invoice();
+$invoice
+    ->setUblVersion('2.1')
+    ->setTipoOperacion('0101')
+    ->setTipoDoc('01')
+    ->setSerie('0001')
+    ->setCorrelativo('1')
+    ->setFechaEmision(new \DateTime())
+    ->setTipoMoneda('PEN')
+    ->setClient($util->getClient())
+    ->setMtoOperGravadas(200)
+    ->setMtoIGV(36)
+    ->setTotalImpuestos(36)
+    ->setValorVenta(200)
+    ->setMtoImpVenta(236)
+    ->setCompany($util->getCompany());
+
+$item1 = new SaleDetail();
+$item1->setCodProducto('P001')
+    ->setUnidad('NIU')
+    ->setDescripcion('PROD 1')
+    ->setCantidad(2)
+    ->setMtoValorUnitario(100)
+    ->setMtoValorVenta(200)
+    ->setMtoBaseIgv(200)
+    ->setPorcentajeIgv(18)
+    ->setIgv(36)
+    ->setTipAfeIgv('10')
+    ->setTotalImpuestos(36)
+    ->setMtoPrecioUnitario(118)
+;
+
+$invoice->setDetails([$item1])
+    ->setLegends([
+        (new Legend())
+            ->setCode('1000')
+            ->setValue('SON DOSCIENTOS TREINTA Y SEIS CON OO/100 SOLES')
+    ]);
+
+// Envio a SUNAT.
+$see = $util->getSee(SunatEndpoints::FE_BETA);
+
+$res = $see->send($invoice);
+$util->writeXml($invoice, $see->getFactory()->getLastXml());
+
+if ($res->isSuccess()) {
+    /**@var $res \Greenter\Model\Response\BillResult*/
+    $cdr = $res->getCdrResponse();
+    $util->writeCdr($invoice, $res->getCdrZip());
+
+    echo $util->getResponseFromCdr($cdr);
+} else {
+    var_dump($res->getError());
+}
+
