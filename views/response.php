e26d51b33ae7464437984a79daf76af1bb37b3e8
Giancarlos Salas <giansalex@gmail.com>
2019-01-22 19:06:33 -0500

    add result view response

---
+++ views/response.php
@@ -0,0 +1,37 @@
+<?php
+/**@var $cdr \Greenter\Model\Response\CdrResponse*/
+
+?>
+<div class="bs-callout <?php echo $cdr->isAccepted() ? 'bs-callout-success' : 'bs-callout-danger' ?>">
+    <h4>Respuesta:</h4><br>
+    <table class="table">
+        <tbody>
+            <tr>
+                <td>ID</td>
+                <td><?=$cdr->getId()?></td>
+            </tr>
+            <tr>
+                <td>Código:</td>
+                <td><?=$cdr->getCode()?></td>
+            </tr>
+            <tr>
+                <td>Descripción:</td>
+                <td><?=$cdr->getDescription()?></td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+    <?php if (count($cdr->getNotes()) > 0) :?>
+        <b>Notas</b><br>
+        <ul class="list-group">
+            <?php foreach ($cdr->getNotes() as $note): ?>
+                <li class="list-group-item"><?=$note?></li>
+            <?php endforeach; ?>
+        </ul><br>
+    <?php endif;?>
+    <b>Adjuntos</b><br>
+    <ul class="list-group">
+        <li class="list-group-item"><a href="files/<?=$filename?>.xml"><?=$filename?>.xml</a></li>
+        <li class="list-group-item"><a href="files/R-<?=$filename?>.zip">R-<?=$filename?>.zip</a></li>
+    </ul>
+</div>
