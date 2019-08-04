41c94f6da4c48055add81a70b959c685240be484
Giancarlos Salas <giansalex@gmail.com>
2019-08-04 15:02:36 -0500

    add ICBPER pdf

--- examples/report/icbper.php
+++ examples/report/icbper.php
@@ -1,12 +1,12 @@
 <?php
 
-use Greenter\Data\Generator\InvoiceStore;
+use Greenter\Data\Generator\InvoiceIcbperStore;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
 $util = Util::getInstance();
 
-$invoice =  $util->getGenerator(InvoiceStore::class)->create();
+$invoice =  $util->getGenerator(InvoiceIcbperStore::class)->create();
 
 try {
     $pdf = $util->getPdf($invoice);
