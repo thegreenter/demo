16148890861baeb68bbe35bcc334a36e5ea3c5e5
Giancarlos Salas <giansalex@gmail.com>
2020-09-01 23:44:32 -0500

    Incluir factura IVAP

---
+++ examples/factura-ivap.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+use Greenter\Model\Response\BillResult;
+use Greenter\Model\Sale\Invoice;
+use Greenter\Model\Sale\SaleDetail;
+use Greenter\Model\Sale\Legend;
+use Greenter\Ws\Services\SunatEndpoints;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$util = Util::getInstance();
+
+$invoice = new Invoice();
+$invoice
+    ->setUblVersion('2.1')
+    ->setTipoOperacion('0101')
+    ->setTipoDoc('01')
+    ->setSerie('F001')
+    ->setCorrelativo('226')
+    ->setFechaEmision(new DateTime())
+    ->setTipoMoneda('PEN')
+    ->setCompany($util->shared->getCompany())
+    ->setClient($util->shared->getClient())
+    ->setMtoBaseIvap(90000.00)
+    ->setMtoIvap(3600.00)
+    ->setTotalImpuestos(3600.00)
+    ->setValorVenta(90000.00)
+    ->setSubTotal(93600.00)
+    ->setMtoImpVenta(93600.00)
+;
+
+$item = new SaleDetail();
+$item->setCodProducto('A001')
+    ->setUnidad('SA')
+    ->setDescripcion('SACOS DE ARROZ')
+    ->setCantidad(900.00)
+    ->setMtoValorUnitario(100.00)
+    ->setMtoValorVenta(90000.00)
+    ->setMtoBaseIgv(90000.00)
+    ->setPorcentajeIgv(4)
+    ->setIgv(3600.00)
+    ->setTipAfeIgv('17')
+    ->setTotalImpuestos(3600.00)
+    ->setMtoPrecioUnitario(104.00)
+;
+
+$invoice->setDetails([$item])
+    ->setLegends([
+        (new Legend())
+            ->setCode('1000')
+            ->setValue('SON TRESCIENTOS TREINTA Y SEIS CON OO/100 SOLES'),
+        (new Legend())
+            ->setCode('2007')
+            ->setValue("Operación sujeta al IVAP"),
+    ]);
+
+// Envio a SUNAT.
+$see = $util->getSee(SunatEndpoints::FE_BETA);
+
+$res = $see->send($invoice);
+$util->writeXml($invoice, $see->getFactory()->getLastXml());
+
+if (!$res->isSuccess()) {
+    echo $util->getErrorResponse($res->getError());
+
+    exit();
+}
+
+/**@var $res BillResult*/
+$cdr = $res->getCdrResponse();
+$util->writeCdr($invoice, $res->getCdrZip());
+
+$util->showResponse($invoice, $cdr);
