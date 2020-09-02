0f3d9db40b75c558bc7391642f905514a6f64f59
Giancarlos Salas <giansalex@gmail.com>
2020-09-02 11:18:27 -0500

    Add greenter icon

--- views/top.php
+++ views/top.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="https://greenter.dev/">
-        <img src="/isolated.svg" height="30" class="d-inline-block align-top" alt="Greenter Logo">
+        <img src="/resources/greenter-icon.svg" height="30" class="d-inline-block align-top" alt="Greenter Logo">
         Greenter
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
