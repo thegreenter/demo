8bb153126ed85b37f580da31e917bfcea9650ee7
Giancarlos Salas <giansalex@gmail.com>
2020-09-18 22:47:08 -0500

    Add invoice send to Bea-tOSE

---
+++ examples/factura-beatose.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+use Greenter\Model\Response\BillResult;
+use Greenter\Model\Sale\Invoice;
+use Greenter\Model\Sale\SaleDetail;
+use Greenter\Model\Sale\Legend;
+use Greenter\Ws\Services\SunatEndpoints;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$util = Util::getInstance();
+
+$invoice = new Invoice();
+$invoice
+    ->setUblVersion('2.1')
+    ->setFecVencimiento(new DateTime())
+    ->setTipoOperacion('0101')
+    ->setTipoDoc('01')
+    ->setSerie('F001')
+    ->setCorrelativo('123')
+    ->setFechaEmision(new DateTime())
+    ->setTipoMoneda('PEN')
+    ->setCompany($util->shared->getCompany())
+    ->setClient($util->shared->getClient())
+    ->setMtoOperGravadas(200)
+    ->setMtoIGV(36)
+    ->setTotalImpuestos(36)
+    ->setValorVenta(300) // Error intencional, valor correcto: 200
+    ->setSubTotal(236)
+    ->setMtoImpVenta(236)
+;
+
+// Detalle gravado
+$item = new SaleDetail();
+$item->setCodProducto('P001')
+    ->setUnidad('NIU')
+    ->setDescripcion('PROD 1')
+    ->setCantidad(2)
+    ->setMtoValorUnitario(100)
+    ->setMtoValorVenta(200)
+    ->setMtoBaseIgv(200)
+    ->setPorcentajeIgv(18)
+    ->setIgv(36)
+    ->setTipAfeIgv('10') // Catalog: 07
+    ->setTotalImpuestos(36)
+    ->setMtoPrecioUnitario(118)
+;
+
+$invoice->setDetails([$item])
+    ->setLegends([
+        (new Legend())
+            ->setCode('1000')
+            ->setValue('SON TRESCIENTOS TREINTA Y SEIS CON OO/100 SOLES')
+    ]);
+
+// Envio a SUNAT.
+$see = $util->getSee('https://beatose.herokuapp.com/ol-ti-itcpe/billService');
+
+$res = $see->send($invoice);
+$util->writeXml($invoice, $see->getFactory()->getLastXml());
+
+if (!$res->isSuccess()) {
+    echo $util->getErrorResponse($res->getError());
+
+    exit();
+}
+
+/**@var $res BillResult*/
+$cdr = $res->getCdrResponse();
+$util->writeCdr($invoice, $res->getCdrZip());
+
+$util->showResponse($invoice, $cdr);
