4c323e2c1131ff24f1048ca60fc7dacc330e0fdd
Giancarlos Salas <giansalex@gmail.com>
2021-03-20 14:37:25 -0500

    Add cdr viewer

---
+++ examples/pages/cdr-viewer.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpZip\ZipFile;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+
+if (!isset($_GET['f'])) {
+    echo 'Parametro f requerido';
+    exit();
+}
+
+$file = $_GET['f'];
+$baseFiles = __DIR__.'/../../files';
+
+$info = pathinfo($file);
+$zipPath = $info['basename'];
+$xmlPath = $info['filename'].'.xml';
+
+if (file_exists($baseFiles.DIRECTORY_SEPARATOR.$xmlPath)) {
+    header('Content-Type: text/xml');
+    readfile($baseFiles.DIRECTORY_SEPARATOR.$xmlPath);
+
+    exit();
+}
+
+if (!file_exists($baseFiles.DIRECTORY_SEPARATOR.$zipPath)) {
+    echo 'Archivo zip no existe';
+    exit();
+}
+
+$zipFile = new ZipFile();
+$zipFile->openFromString(file_get_contents($baseFiles.DIRECTORY_SEPARATOR.$zipPath));
+$zipFile->extractTo($baseFiles);
+
+header('Content-Type: text/xml');
+readfile($baseFiles.DIRECTORY_SEPARATOR.$xmlPath);
