948cfebbedeb99c73b9c9308365b6b37670f0613
Giancarlos Salas <giansalex@gmail.com>
2021-05-11 16:11:43 -0500

    Update navbar css

--- views/top.php
+++ views/top.php
@@ -1,32 +1,42 @@
+<style>
+    .jumbotron {
+        padding: 2rem 1rem;
+        margin-bottom: 2rem;
+        background-color: #e9ecef;
+        border-radius: .3rem;
+    }
+</style>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="https://greenter.dev/">
-        <img src="/resources/greenter-icon.svg" height="30" class="d-inline-block align-top" alt="Greenter Logo">
-        Greenter
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="https://greenter.dev/">
+            <img src="/resources/greenter-icon.svg" height="30" class="d-inline-block align-top" alt="Greenter Logo">
+            Greenter
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://community.greenter.dev/" target="_blank"><i class="fab fa-discourse text-success"></i> Forum</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://fb.me/thegreenter" target="_blank"><i class="fab fa-facebook text-info"></i> Facebook</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://fe-primer.greenter.dev/" target="_blank"><img src="https://fe-primer.greenter.dev/img/logo.svg" height="20"> Fe Primer</a>
-            </li>
-        </ul>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="https://github.com/thegreenter/demo"><i class="fab fa-github"></i> @greenter/demo</a>
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://community.greenter.dev/" target="_blank"><i class="fab fa-discourse text-success"></i> Forum</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://fb.me/thegreenter" target="_blank"><i class="fab fa-facebook text-info"></i> Facebook</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://fe-primer.greenter.dev/" target="_blank"><img src="https://fe-primer.greenter.dev/img/logo.svg" height="20"> Fe Primer</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/thegreenter/demo"><i class="fab fa-github"></i> @greenter/demo</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 <div class="jumbotron text-center">
