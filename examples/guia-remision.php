3fb706b1bb143cd890464b966727a2c57f6cef84
Giancarlos Salas <me@giansalex.dev>
2022-12-23 13:55:24 -0500

    Update guia 2022

--- examples/guia-remision.php
+++ examples/guia-remision.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use Greenter\Model\Client\Client;
+use Greenter\Model\Despatch\AdditionalDoc;
 use Greenter\Model\Despatch\Despatch;
 use Greenter\Model\Despatch\DespatchDetail;
 use Greenter\Model\Despatch\Direction;
 use Greenter\Model\Despatch\Shipment;
 use Greenter\Model\Despatch\Transportist;
 use Greenter\Model\Response\BillResult;
+use Greenter\Model\Response\CdrResponse;
 use Greenter\Model\Sale\Document;
 use Greenter\Ws\Services\SunatEndpoints;
 
@@ -16,36 +18,34 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $util = Util::getInstance();
 
-$rel = new Document();
-$rel->setTipoDoc('02') // Cat. 21 - Numero de Orden de Entrega
-->setNroDoc('213123');
+$rel = new AdditionalDoc();
+$rel->setTipoDesc('Factura')
+    ->setTipo('01') // Cat. 61 - Factura
+    ->setNro('F001-222')
+    ->setEmisor('20123456789');
 
 $transp = new Transportist();
 $transp->setTipoDoc('6')
     ->setNumDoc('20000000002')
     ->setRznSocial('TRANSPORTES S.A.C')
-    ->setPlaca('ABI-453')
-    ->setChoferTipoDoc('1')
-    ->setChoferDoc('40003344');
+    ->setNroMtc('0001');
 
 $envio = new Shipment();
 $envio
     ->setCodTraslado('01') // Cat.20
     ->setDesTraslado('VENTA')
-    ->setModTraslado('01') // Cat.18
+    ->setModTraslado('01') // Cat.18 - Transp. Publico
     ->setFecTraslado(new DateTime())
-    ->setCodPuerto('123')
-    ->setIndTransbordo(false)
     ->setPesoTotal(12.5)
     ->setUndPesoTotal('KGM')
 //    ->setNumBultos(2) // Solo válido para importaciones
-    ->setNumContenedor('XD-2232')
     ->setLlegada(new Direction('150101', 'AV LIMA'))
     ->setPartida(new Direction('150203', 'AV ITALIA'))
     ->setTransportista($transp);
 
 $despatch = new Despatch();
-$despatch->setTipoDoc('09')
+$despatch->setVersion('2022')
+    ->setTipoDoc('09')
     ->setSerie('T001')
     ->setCorrelativo('123')
     ->setFechaEmision(new DateTime())
@@ -53,13 +53,9 @@ $despatch->setTipoDoc('09')
     ->setDestinatario((new Client())
         ->setTipoDoc('6')
         ->setNumDoc('20000000002')
-        ->setRznSocial('EMPRESA (<!-- --> />) 1'))
-    ->setTercero((new Client())
-        ->setTipoDoc('6')
-        ->setNumDoc('20000000003')
-        ->setRznSocial('EMPRESA SA'))
+        ->setRznSocial('EMPRESA DEST 1'))
     ->setObservacion('NOTA GUIA')
-    ->setRelDoc($rel)
+    ->setAddDocs([$rel])
     ->setEnvio($envio);
 
 $detail = new DespatchDetail();
@@ -67,24 +63,20 @@ $detail->setCantidad(2)
     ->setUnidad('ZZ')
     ->setDescripcion('PROD 1')
     ->setCodigo('PROD1')
-    ->setCodProdSunat('P001');
+    ->setCodProdSunat('10101508');
 
 $despatch->setDetails([$detail]);
 
 // Envio a SUNAT.
 $see = $util->getSee(SunatEndpoints::GUIA_BETA);
 
-$res = $see->send($despatch);
-$util->writeXml($despatch, $see->getFactory()->getLastXml());
+$xml = $see->getXmlSigned($despatch);
+$util->writeXml($despatch, $xml);
 
-if ($res->isSuccess()) {
-    /**@var $res BillResult*/
-    $cdr = $res->getCdrResponse();
-    $util->writeCdr($despatch, $res->getCdrZip());
-
-    $util->showResponse($despatch, $cdr);
-} else {
-    echo $util->getErrorResponse($res->getError());
-}
+$cdr = (new CdrResponse())
+    ->setCode('-')
+    ->setDescription('XML valido')
+    ->setNotes([]);
+$util->showResponse($despatch, $cdr);
 
 
